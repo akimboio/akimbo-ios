c27ad0ff9769c18785ff9b74b279e9dff13a7088
Wes Okes <wokes@lamppostgroup.com>
2012-12-03 16:31:19 -0500

    * Moved files into directories

---
+++ Code/Data/AkimboDataStoreDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AkimboDataStoreDelegate.h
+//  akimbo-ios
+//
+//  Created by Wes Okes on 12/3/12.
+//  Copyright (c) 2012 Akimbo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AkimboDataStoreDelegate : NSObject
+
+@end
